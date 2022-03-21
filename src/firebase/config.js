0f6ea027b173558de4c378856a26af77c4f701a9
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-03-21 13:49:53 -0500

    my first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMvAExsxBKCNaVaxbc_aDTaUp9-_Z5fH4",
+  authDomain: "piddletogether.firebaseapp.com",
+  projectId: "piddletogether",
+  storageBucket: "piddletogether.appspot.com",
+  messagingSenderId: "863572980550",
+  appId: "1:863572980550:web:abb276d10aaee34beeab47",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
