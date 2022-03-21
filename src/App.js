7e460c35d5a319327e67cd8661d9a09aafd0d911
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-03-21 14:59:18 -0500

    added navbar and sidebar components and site pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles:
+import "./App.css";
+
+// pages & components:
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import "./Dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
