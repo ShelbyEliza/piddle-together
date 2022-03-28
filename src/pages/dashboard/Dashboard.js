d14d4d5a7bcb9004e135e8eec1fea2f1100500ca
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-03-28 15:16:30 -0500

    added comments and filter option. Right before initial deployment

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,17 +1,57 @@
 // styles:
 import "./Dashboard.css";
 
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
 
 export default function Dashboard() {
+  const [currentFilter, setCurrentFiler] = useState("all");
   const { documents, error } = useCollection("projects");
+  const { user } = useAuthContext();
+
+  const changeFilter = (newFilter) => {
+    setCurrentFiler(newFilter);
+  };
+
+  const filteredProjects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {filteredProjects && <ProjectList projects={filteredProjects} />}
     </div>
   );
 }
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,27 @@
+// styles:
 import "./Project.css";
 
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+import ProjectSummary from "./ProjectSummary";
+import ProjectComments from "./ProjectComments";
+
 export default function Project() {
+  // named id in app route setup
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Project</h1>
+    <div className="project-details">
+      <ProjectSummary project={document} />
+      <ProjectComments project={document} />
     </div>
   );
 }
